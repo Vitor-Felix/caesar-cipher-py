6604a811be135f605ead9fda54627d981187aca8
Vitor-Felix <vytorfelix@gmail.com>
2020-04-25 02:05:37 -0300

    Finishing code_decode

---
+++ coder_decoder.py
@@ -0,0 +1,53 @@
+#!/usr/bin/env python
+
+import argparse
+
+
+def coder(phrase, rotations=3):
+    
+    coded_phrase = ""
+
+    for letter in phrase:
+        if ord(letter) > 96 and ord(letter) < 123: 
+            if (ord(letter) + rotations) > 122:
+                coded_phrase += chr((ord(letter) + rotations) - 26)
+            else:
+                coded_phrase += chr(ord(letter) + rotations)
+        else:
+            coded_phrase += letter
+
+    return coded_phrase
+
+
+def decoder(phrase, rotations=3):
+    
+    decoded_phrase = ""
+
+    for letter in phrase:
+        if ord(letter) > 96 and ord(letter) < 123: 
+            if (ord(letter) - rotations) < 97:
+                decoded_phrase += chr((ord(letter) - rotations) + 26)
+            else:
+                decoded_phrase += chr(ord(letter) - rotations)
+        else:
+            decoded_phrase += letter
+
+    return decoded_phrase
+
+
+def main():
+    roles = {'code': coder, 'decode': decoder}
+    parser = argparse.ArgumentParser(description='Code/Decode a Rot-N Cipher')
+    parser.add_argument('role', choices=roles, 
+                        help='Choose between code or decode the phrase')
+    parser.add_argument('phrase', help='Phrase to be coded or decoded. Use Quotes!')
+    parser.add_argument('-r', '--rotations', default=3, help='Number of shifts.\n' 
+                        'Default is the Caesar\'s cipher (Rot-3)')
+    args = parser.parse_args()
+    function = roles[args.role]
+
+    print(function(args.phrase.lower(), int(args.rotations)))
+
+
+if __name__ == '__main__':
+    main()
---
+++ main.py
@@ -0,0 +1,6 @@
+#!/usr/bin/env python
+def main():
+    print('K.O.')
+
+if __name__ == "__main__":
+    main()
