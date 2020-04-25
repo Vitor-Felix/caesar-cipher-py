417f3e59d5b588aea013118dad3cefa4bea592bf
Vitor-Felix <vytorfelix@gmail.com>
2020-04-25 02:45:18 -0300

    testing vscode files

--- main.py
+++ main.py
@@ -1,6 +1,12 @@
 #!/usr/bin/env python
-def main():
-    print('K.O.')
 
-if __name__ == "__main__":
-    main()
+from coder_decoder import decoder
+
+
+print(decoder("d oljhlud udsrvd pduurp vdowrx vreuh r fdfkruur fdqvdgr"))
+
+# def main():
+#     print('K.O.')
+
+# if __name__ == "__main__":
+#     main()
